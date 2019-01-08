608de8a04ab396dbc8d3f563cf6290b07d043475
Jie <jzhu@tripadvisor.com>
2019-01-08 12:23:58 -0500

    heroku build update

---
+++ build/precache-manifest.1afd28dc450f764935184b9c6ad4af26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d57dcb97ad078327d9b3",
+    "url": "/static/js/main.d57dcb97.chunk.js"
+  },
+  {
+    "revision": "4a83cc11ee5fbefa1cdf",
+    "url": "/static/js/1.4a83cc11.chunk.js"
+  },
+  {
+    "revision": "d57dcb97ad078327d9b3",
+    "url": "/static/css/main.9447bdfe.chunk.css"
+  },
+  {
+    "revision": "4a83cc11ee5fbefa1cdf",
+    "url": "/static/css/1.f19de2c6.chunk.css"
+  },
+  {
+    "revision": "f245a4d2c8401d875e11890271fc91fe",
+    "url": "/index.html"
+  }
+];
